897c7b30f76e799ff8a3fda2fc1463f3b6f3fadc
Filip Kobiec <fipis14@gmail.com>
2020-12-22 15:52:35 +0100

    Player can now throw grenade

---
+++ BulletDancer2/Assets/_Scripts/Player/Inventory/ThrowableItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts.Player.Inventory
+{
+    public interface ThrowableItem
+    {
+        void Throw(float speed, Vector2 itemDirection, Vector2 playerPosition);
+        void SetButtonStatus(ThrowableItem throwableItem, bool isActive);
+    }
+}
