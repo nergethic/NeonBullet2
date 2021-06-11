0a262b233b6c22524793b319466d0ae01a998279
Filip Kobiec <fipis14@gmail.com>
2021-06-11 12:10:51 +0200

    Check collider of room to BoxCollider2d

--- BulletDancer2/Assets/_Scripts/Rooms/Room.cs
+++ BulletDancer2/Assets/_Scripts/Rooms/Room.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class Room : MonoBehaviour {
     [SerializeField] public RoomType roomType;
     [SerializeField] public DoorData[] doorsData;
-    [SerializeField] private BoxCollider collider;
+    [SerializeField] private BoxCollider2D collider;
 
-    public Bounds GetBounds() => collider.bounds;
+    public Vector2 GetSize() => collider.size;
 }
 
 [Serializable]
