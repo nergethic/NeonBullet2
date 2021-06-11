60caaad208a0a996f43fcac17f0fcbb184759d83
Filip Kobiec <fipis14@gmail.com>
2021-06-11 12:09:15 +0200

    Add reverse direction extention method to DoorDirection

---
+++ BulletDancer2/Assets/_Scripts/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets._Scripts
+{
+    public static class Util
+    {
+        public static DoorDirection GetOppositeDirection(this DoorDirection direction) =>
+            direction switch
+            {
+                DoorDirection.Top => DoorDirection.Down,
+                DoorDirection.Down => DoorDirection.Top,
+                DoorDirection.Left => DoorDirection.Right,
+                DoorDirection.Right => DoorDirection.Left
+            };
+	}
+}
+
