4693a1cc575f80bcb41626df94851cff10943612
Filip Kobiec <fipis14@gmail.com>
2022-01-01 19:55:32 +0100

    Add UIMAnager

---
+++ BulletDancer2/Assets/_Scripts/Managers/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets._Scripts.Player.UI
+{
+    public class UIManager : SceneManager
+    {
+        [SerializeField] List<UIPanel> uiPanels;
+        private UIPanel activePanel;
+        public UIPanel ActivePanel => activePanel;
+        public bool IsPanelActive => activePanel != null;
+
+        public override void Init(MasterSystem masterSystem, SceneManagerData data)
+        {
+            base.Init(masterSystem, data);
+            data.playerController.InitializeUIManager(this);
+            ChangeInitializationState(ManagerInitializationState.COMPLETED);
+        }
+
+        public void ShowPanel(PanelType type)
+        {
+            foreach (var panel in uiPanels)
+            {
+                if (panel.panelType == type)
+                {
+                    panel.ShowPanel();
+                    activePanel = panel;
+                    break;
+                }
+            }
+        }
+        
+        public void HidePanel()
+        {
+            ActivePanel?.HidePanel();
+            activePanel = null;
+        }
+
+        public override void Tick(float dt){}
+    }
+}
