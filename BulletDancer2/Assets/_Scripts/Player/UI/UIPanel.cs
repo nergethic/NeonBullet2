97f78b092285a7b2a090ddecb67a89bb54900931
Filip Kobiec <fipis14@gmail.com>
2022-01-02 19:11:46 +0100

    Init UIPanel with MasterSystem

--- BulletDancer2/Assets/_Scripts/Player/UI/UIPanel.cs
+++ BulletDancer2/Assets/_Scripts/Player/UI/UIPanel.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public abstract class UIPanel : MonoBehaviour
 {
+    protected MasterSystem _masterSystem;
+    public void Initialize(MasterSystem masterSystem) => _masterSystem = masterSystem;
     public PanelType panelType;
     public void ShowPanel() => gameObject.SetActive(true);
     public void HidePanel() => gameObject.SetActive(false);
