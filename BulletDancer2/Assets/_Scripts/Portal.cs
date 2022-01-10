8a667c70efa4fba8e5bcf21322718dfc38d889a5
Kamil Jamróz <nergydev@gmail.com>
2022-01-10 10:39:04 +0100

    portal interaction

--- BulletDancer2/Assets/_Scripts/Portal.cs
+++ BulletDancer2/Assets/_Scripts/Portal.cs
@@ -1,14 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
-public class Portal : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            collision.gameObject.transform.position = new Vector2(0, 0);
+public class Portal : MonoBehaviour {
+    [SerializeField] VisualEffect vfx;
+    [SerializeField] float detectionRadius = 3f;
+    
+    const string playerTagName = "Player";
+    PlayerController player;
+    bool isPlaying;
+
+    void Awake() {
+        vfx.enabled = true;
+        vfx.Stop();
+        isPlaying = false;
+    }
+
+    void Update() {
+        if (player == null) {
+            player = FindObjectOfType<PlayerController>(); // TODO: create vfx system that has a ref to player
+            if (player == null)
+                return;
         }
+        
+        var diff = player.transform.position - transform.position;
+        if (isPlaying) {
+            if (diff.sqrMagnitude > detectionRadius + 0.1f) {
+                vfx.Stop();
+                isPlaying = false;
+            }
+        } else {
+            if (diff.sqrMagnitude < detectionRadius) {
+                vfx.Play();
+                isPlaying = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag(playerTagName))
+            collision.gameObject.transform.position = Vector2.zero;
     }
 }
