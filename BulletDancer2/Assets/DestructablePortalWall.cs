093146524c0cca56511abfd63f4a36f32611b3de
Filip Kobiec <fipis14@gmail.com>
2022-01-27 16:19:15 +0100

    Add new explosion effect

--- BulletDancer2/Assets/DestructablePortalWall.cs
+++ BulletDancer2/Assets/DestructablePortalWall.cs
@@ -2,11 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestructablePortalWall : DestructableWall
+public class DestructablePortalWall : MonoBehaviour
 {
     [SerializeField] Portal portal;
+    [SerializeField] GameObject effect;
+    [SerializeField] List<Sprite> sprites;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    List<int> projectilesEntered = new List<int>();
+    int levelOfDestruction;
+
+    private void Awake()
+    {
+        levelOfDestruction = sprites.Count;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+
+        var bullet = other.GetComponent<Projectile>();
+        if (bullet == null)
+            return;
+
+        if (bullet.Type() != ProjectileType.Grenade)
+            return;
+
+        int id = bullet.GetHashCode();
+        if (projectilesEntered.Contains(id))
+            return;
+
+        projectilesEntered.Add(id);
+
+
+        if (levelOfDestruction != 0)
+        {
+            levelOfDestruction -= bullet.projectileData.damage;
+
+            if (levelOfDestruction <= 0)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            spriteRenderer.sprite = sprites[levelOfDestruction - 1];
+        }
+    }
     private void OnDestroy()
     {
+        var newEffect = Instantiate(effect);
+        newEffect.transform.position = gameObject.transform.position;
         var spawnedPortal = Instantiate(portal);
         spawnedPortal.transform.position = gameObject.transform.position;
     }
